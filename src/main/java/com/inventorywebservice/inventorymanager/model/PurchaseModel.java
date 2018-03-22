d170417a3c2e2f704e7df52e4b8b4a2293167973
Abhijeet Rathore <abhijeet.rathore>
2018-03-22 19:51:48 +0530

    Added Skeleton for Purchase API.

---
+++ src/main/java/com/inventorywebservice/inventorymanager/model/PurchaseModel.java
@@ -0,0 +1,5 @@
+package com.inventorywebservice.inventorymanager.model;
+
+public class PurchaseModel {
+
+}
---
+++ src/main/java/com/inventorywebservice/inventorymanager/service/PurchaseServiceImpl.java
@@ -0,0 +1,19 @@
+package com.inventorywebservice.inventorymanager.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.inventorywebservice.inventorymanager.model.PurchaseModel;
+
+
+@Service
+public class PurchaseServiceImpl implements PurchaseService {
+	
+	public List<PurchaseModel> findAllPurchasesByUser(String userEmail)
+	{
+		
+		return null;
+	}
+
+}
